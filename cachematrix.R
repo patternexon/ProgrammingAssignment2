bdae257bacf1d1c64e8955db2a2c40f7b3de9bd0
Akshat R. Shrivastava <patternexon@gmail.com>
2014-04-27 22:33:54 -0400

    Implemented cacheSolve - an easier way to solve for inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Methods here provide the ability solve for the inverse of matrices faster 
+## by caching the inverse of the matrix
 
-## Write a short comment describing this function
+## Creates a modified Matrix whose inverse is easily cached 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <-function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list( set = set, get = get, 
+        setinverse = setinverse, 
+        getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+ 
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
